83ae5900657b66335a40851da83273f217d7440d
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-22 15:42:49 +0200

    removed spaces
    Time to find a decent linter

--- L12_1_Eerste_JS/script.js
+++ L12_1_Eerste_JS/script.js
@@ -1,11 +1,11 @@
 console.log("Hello Winc Academy");
-      let name = "Winc";
-      console.log(name);
-      // let if = "in-use"; [index.html:14 Uncaught SyntaxError: Unexpected token 'if'] if is een reserved keyword
-      console.log(4 + 4); // 8
-      console.log("4" + "4"); // 44
-      console.log(((16 - 7) / 18 + 22) % 20); // 9/18 =.5 +22 = 22.5 % 20 > 'rest' 2.5
-      let age = 356;
-      console.log(typeof age); // number
-      let ageasstring = "356";
-      console.log(typeof ageasstring); // string
+let name = "Winc";
+console.log(name);
+// let if = "in-use"; [index.html:14 Uncaught SyntaxError: Unexpected token 'if'] if is een reserved keyword
+console.log(4 + 4); // 8
+console.log("4" + "4"); // 44
+console.log(((16 - 7) / 18 + 22) % 20); // 9/18 =.5 +22 = 22.5 % 20 > 'rest' 2.5
+let age = 356;
+console.log(typeof age); // number
+let ageasstring = "356";
+console.log(typeof ageasstring); // string
