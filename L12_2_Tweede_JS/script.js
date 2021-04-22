436bd2d550d2bc368f3308ad192dc8841d96a23b
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-22 16:00:55 +0200

    L12_2 (2)
    forgot to save...

--- L12_2_Tweede_JS/script.js
+++ L12_2_Tweede_JS/script.js
@@ -2,3 +2,6 @@ console.log("Hallo Winc Academy studenten");
 // Dit is een grote som (maar dan een vermenigvuldiging)
 console.log(1230941 * 1823794);
 console.log(637263 / 54);
+
+/* console.log(1230941 * 1823794);
+console.log(637263 / 54); */
