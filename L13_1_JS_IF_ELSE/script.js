57700071cb5b5e94fec10ea8d48a7825b04782e4
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-22 17:03:26 +0200

    L13_1

---
+++ L13_1_JS_IF_ELSE/script.js
@@ -0,0 +1,18 @@
+const age = 19;
+const isFemale = true;
+const driverStatus = "notbob";
+if (age >= 18) {
+  console.log("Kom maar binnen!");
+} else {
+  console.log("HO!");
+}
+if (isFemale) {
+  console.log("You're female");
+} else {
+  console.log("You're not female");
+}
+if (driverStatus == "bob") {
+  console.log("You may drive!");
+} else {
+  console.log("You're not allowed to drive");
+}
