fd6f1f7f957baf3cb6b5c80fa33a4c29c5aeaa73
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-23 17:24:21 +0200

    L14_2

--- L14_2_Function_Arguments/wallPaint.js
+++ L14_2_Function_Arguments/wallPaint.js
@@ -1,7 +1,8 @@
 const paintWall = function (color, defineWall) {
   console.log("The " + defineWall + " wall is painted " + color);
-};
+}; // the order of the arguments in the function(x, y) doesn't matter, the names matter
+// the order here defines the order the arguments are read when called (and thus which argument is set to what value)
 
 paintWall("green", "southern");
 paintWall("red", "south-east");
-paintWall("south-east", "red");
+paintWall("south-east", "red"); // the order matters of-course
