44f0375dde377b26c97d03820ce0cba13720fd58
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-23 16:23:52 +0200

    L14_1

---
+++ L14_1_JS_Function_Basics/pizza.js
@@ -0,0 +1,33 @@
+const stepsPizza = function () {
+  console.log("Mix the dough");
+  console.log("Kneed the dough");
+  console.log("Pre-heat the oven");
+  console.log("Roll-out the dough");
+  console.log("Put sauce on the rolled out dough");
+  console.log("Put toppings on the sauce");
+  console.log("Add (extra) cheese on top");
+  console.log("Bake in the oven till perfection!");
+  console.log("Take the pizza out of the oven!!");
+};
+
+const stepsSushi = function () {
+  console.log("Boil the rice");
+  console.log("Cut fish in pieces");
+  console.log("Drain the rice");
+  console.log("Cool down the rice");
+  console.log("Mix vinegar with the rice");
+  console.log("Roll out a nori sheet on sushi-roller");
+  console.log("Spread rice on nori sheet");
+  console.log("Put fish pieces on rice");
+  console.log("Add wasabi");
+  console.log("Add mayo");
+  console.log("Roll tight");
+  console.log("Remove sushi-roller");
+  console.log("Cut roll in edible pieces");
+};
+// calling the function
+stepsSushi();
+stepsSushi();
+stepsSushi();
+stepsPizza();
+stepsPizza();
