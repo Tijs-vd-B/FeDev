4bb393514de1486d8e43c1113e1593b2cfa4e049
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-23 17:15:02 +0200

    L14_2

---
+++ L14_2_Function_Arguments/wallPaint.js
@@ -0,0 +1,7 @@
+const paintWall = function (color, defineWall) {
+  console.log("The " + defineWall + " wall is painted " + color);
+};
+
+paintWall("green", "southern");
+paintWall("red", "south-east");
+paintWall("south-east", "red");
