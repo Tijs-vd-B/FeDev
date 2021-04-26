7b4e44f08ff5ddfdd9809114aae5b72fbb682a81
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-26 13:56:17 +0200

    erroneous average.js

--- L14_3_Function_Return_Statements/average.js
+++ L14_3_Function_Return_Statements/average.js
@@ -1,25 +1,17 @@
-// function average(fiveNumbers) {
-//   return Math.round(
-//     //   return (
-//     (fiveNumbers[0] +
-//       fiveNumbers[1] +
-//       fiveNumbers[2] +
-//       fiveNumbers[3] +
-//       fiveNumbers[4]) /
-//       5
-//     //   fiveNumbers.length // > false result if using more or less than 5 numbers
-//   );
-// }
-
 function average(fiveNumbers) {
   return Math.round(
     //   return (
-    total(fiveNumbers) / 5
+    (fiveNumbers[0] +
+      fiveNumbers[1] +
+      fiveNumbers[2] +
+      fiveNumbers[3] +
+      fiveNumbers[4]) /
+      5 // static
     //   fiveNumbers.length // > false result if using more or less than 5 numbers
   );
 }
 
-console.log(average([1, 2, 3, 4, 4, 5]));
+console.log(average([1, 2, 3, 4, 4, 5])); // correct, uses the first five numbers
 console.log(average([111, 2222, 333, 44, 65]));
 console.log(average([10, 21, 32, 43, 54]));
 console.log(average([-19, -2, 30, -4, -6]));
