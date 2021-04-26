0b82963cccf74639d0aee2454aa86ed53f63112c
Tijs-vd-B <81232322+Tijs-vd-B@users.noreply.github.com>
2021-04-26 18:01:01 +0200

    L14_6

---
+++ L14_6_For_Loops/multiplication-tables.js
@@ -0,0 +1,9 @@
+// tafel van 9
+// for (i = 1; i <= 10; i++) {
+//   console.log(i + " x 9 = " + i * 9);
+// }
+
+// nested for loop; tafel van 1 t/m 10
+for (i = 1; i <= 10; i++) {
+  for (x = 1; x <= 10; x++) console.log(x + " x " + i + " = " + x * i);
+}
